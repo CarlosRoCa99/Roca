62aa6412b86fb1f20b21f672fd5dcf18179649a4
CarlosRoCa99 <isaacro007@hotmail.com>
2017-03-03 20:53:25 -0400

    Update tarearadaresdetramo.c

--- tarearadaresdetramo.c
+++ tarearadaresdetramo.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 int main () {
-	int maximav,distancia,tiempo;
+	float maximav,distancia,tiempo,puntos;
 	do {
 	printf ("Ingrese la velocidad maxima (Km/h): ");
-	scanf ("%i",&maximav);
+	scanf ("%f",&maximav);
 	printf ("Ingrese la distancia entre cada camara (metros): ");
-	scanf ("%i",&distancia);
+	scanf ("%f",&distancia);
 	printf ("Ingrese el tiempo transcurrido en el cual el vehiculo paso por la primera camara y luego por la segunda (segundos): ");
-	scanf ("%i",&tiempo);
+	scanf ("%f",&tiempo);
+	puntos = maximav * 1.2;
 	if(maximav==0 && distancia==0 && tiempo==0){
 		printf ("\n\nHasta luego.\n\n");
 	}
+	else if (maximav < 0 || distancia < 0 || tiempo < 0){
+		printf ("\n\nERROR\n\n");
+	}
 	else if (((distancia/tiempo)*3600/1000)<=maximav){
 		printf ("\n\nOK\n\n");
+	}
+	else if (((distancia/tiempo)*3600/1000)>puntos){
+		printf ("\n\nPUNTOS\n\n");
 	}
 	else if (((distancia/tiempo)*3600/1000)>maximav){
 		printf ("\n\nMULTA\n\n");
